96017a8e5325a3a2643546c4c632f5a3b200ad1f
obvoso <autumninmoon@gmail.com>
2024-12-14 15:20:05 +0900

    feat: add theme provider

---
+++ src/styles/theme.js
@@ -0,0 +1,36 @@
+const fontSize = {
+  xs: '0.5rem',
+  sm: '0.75rem',
+  base: '1rem',
+  md: '1.25rem',
+  lg: '1.5rem',
+  xl: '2rem',
+  '2xl': '2.5rem',
+}
+
+const fontWeight = {
+  light: 300,
+  regular: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+  extraBold: 800,
+  black: 900,
+}
+
+const color = {
+  primary: '#F95E07',
+  text: '#000000',
+  textSecondary: '#797B7E',
+  background: '#F5F5F6',
+  backgroundSecondary: '#FDFDFD',
+  success: '#87B6A1',
+  reject: '#DB79A9',
+  border: 'rgba(0, 0, 0, 0.46)',
+}
+
+export const theme = {
+  fontSize,
+  fontWeight,
+  color,
+}
